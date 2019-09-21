6b3a48a431d87f49ffd6a0d209a9415e48ee9884
Daniel Lamb <daniellamb113@gmail.com>
2019-09-21 14:37:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,18 @@
 def roll_call_dwarves
 puts ["Doc", "Dopey", "Bashful", "Grumpy"]
+
 end
 
 def summon_captain_planet
 puts planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
 end
 
 def long_planeteer_calls
 short_words = ["puff", "go", "two"]
 long_planeteer_calls(short_words)
+
+end
 
  
 assorted_words = ["two", "go", "industrious", "bop"]
