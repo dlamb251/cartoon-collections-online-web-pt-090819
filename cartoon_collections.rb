3467138ad911a2122eb02224fcd3ce7ea6376a68
Daniel Lamb <daniellamb113@gmail.com>
2019-09-21 14:35:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,13 @@ puts planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 end
 
 def long_planeteer_calls# code an argument here
-
+short_words = ["puff", "go", "two"]
+long_planeteer_calls(short_words)
+#=> false
+ 
+assorted_words = ["two", "go", "industrious", "bop"]
+long_planeteer_calls(assorted_words)
+#=> true
 end
 
 def find_the_cheese# code an argument here
